736ea8b0c2b135cc4bb74a97e5142d9bebeb39cf
sandymosaad <sandyzakher5@gmail.com>
2025-06-24 16:31:23 +0200

    Initial Angular project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { WeatherDetailsComponent } from './components/weather-details/weather-details.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'weather/:city', component: WeatherDetailsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: '**', component: NotFoundComponent },
+];
