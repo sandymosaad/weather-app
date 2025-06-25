619308ac9f3bf94383d89ed68dc97b72f2a2ee90
sandymosaad <sandyzakher5@gmail.com>
2025-06-25 14:57:12 +0200

    added spinner when user search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -21,27 +21,33 @@ export class SearchComponent {
   searchTerm: string='';
   weatherData!:WeatherData |undefined ;
   errorMessage:string ='';
+  isLoading: boolean = false;
 
   constructor(private _weatherService: WeatherService) {}
 
   onSearch(){
+    this.isLoading = true;
 
-  this._weatherService.getWeather(this.searchTerm).subscribe({
-    next: (data) => {
-      console.log('Weather Data:', data);
-      this.weatherData=data;
-      this.errorMessage=''
-    },
-    error: (err) => {
-      if (err.status === 404) {
-      this.weatherData = undefined;
-        this.errorMessage='City not found';
-        console.log('City not found');
-      } else {
-        console.error('Error fetching weather:', err);
+    this._weatherService.getWeather(this.searchTerm).subscribe({
+      next: (data) => {
+        console.log('Weather Data:', data);
+        this.weatherData=data;
+        this.errorMessage='';
+        this.isLoading = false;
+      },
+      error: (err) => {
+        if (err.status === 404) {
+        this.weatherData = undefined;
+          this.errorMessage='City not found';
+          this.isLoading = false;
+          console.log('City not found');
+        } else {
+          console.error('Error fetching weather:', err);
+          this.errorMessage = 'Something went wrong. Please try again.';
+          this.isLoading = false;
+        }
       }
-    }
-  });
+    });
 
 }
 clearError() {
